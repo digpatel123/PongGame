2c75427162e826e8ae9149c5a353236de336b62c
digpatel123 <digpatel123@gmail.com>
2023-03-20 14:50:12 +0000

    Created the ball class and detection of the ball with the walls and paddles.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
-from paddle import paddle
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -7,13 +9,12 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-left_paddle = paddle(-350, 0)
-right_paddle = paddle(350, 0)
+left_paddle = Paddle(-350, 0)
+right_paddle = Paddle(350, 0)
+ball = Ball()
 screen.update()
 
-
 game_is_on = True
-
 
 screen.listen()
 screen.onkey(left_paddle.go_down, "s")
@@ -22,7 +23,16 @@ screen.onkey(right_paddle.go_down, "Down")
 screen.onkey(right_paddle.go_up, "Up")
 
 while game_is_on:
+    time.sleep(0.1)
+    ball.move()
     screen.update()
 
+    # Detect the collision with  upper amd lower walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with the paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 330 or ball.distance(left_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
 
 screen.exitonclick()
